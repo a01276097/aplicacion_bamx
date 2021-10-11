10790acbf08f7fed8535c561642293646f942e0f
a01276097 <a01276097@itesm.mx>
2021-10-11 08:17:42 -0500

    anadidos activities a los layouts faltantes

---
+++ app/src/main/java/com/example/aplicacion_bamx/Activity_general_login.kt
@@ -0,0 +1,11 @@
+package com.example.aplicacion_bamx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_general_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.general_login)
+    }
+}
